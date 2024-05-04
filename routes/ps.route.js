bdf8c4c9a2744c9e167343cb6c272689dc4d8a3b
Thanadetch Lertwuthikarn <tlertwuthikarn@palo-it.com>
2024-05-04 12:04:04 +0700

    update ps api

--- routes/ps.route.js
+++ routes/ps.route.js
@@ -1,22 +1,6 @@
 const express = require("express");
 const {getAvailableFromPsCode, getPageInstance} = require("../controllers/ps.controller");
-const {getBrowser, getPageAndSignIn} = require("../utils/scrapingUtils");
 const router = express.Router()/* GET users listing. */
-
-router.get('/setup/browser', async function (req, res, next) {
-    await getBrowser()
-    res.send({
-        data: true
-    });
-});
-
-router.get('/setup/signIn', async function (req, res, next) {
-    getPageAndSignIn()
-    res.send({
-        data: true
-    });
-});
-
 
 router.get('/available/:psCode', async function (req, res, next) {
     const {psCode} = req.params
